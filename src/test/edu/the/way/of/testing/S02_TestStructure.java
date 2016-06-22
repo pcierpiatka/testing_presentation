663f8fceaec0daf9c8e46b3e59e994145e8c226f
pcierpiatka <p.cierpiatka@gmail.com>
2016-06-23 00:45:02 +0200

    test structure

--- src/test/edu/the/way/of/testing/S02_TestStructure.java
+++ src/test/edu/the/way/of/testing/S02_TestStructure.java
@@ -9,22 +9,30 @@ import org.junit.Test;
 public class S02_TestStructure {
 
     @Test
-    public void should_(){
+    public void testJUnitWay(){
         //arrange
         //act
         //assert
     }
 
-    public void should_myWay() {
+    public void shouldDoTdd() {
         //given
         //when
         //then
     }
 
-    public void should_whateva() {
-        //prepar test env
+    public void whatever_works() {
+        //prepare test env
         //execute behavior
         //verify expected result
+    }
+
+    public void should_description_of_required_behavior() {
+        //given
+
+        //when
+
+        //them
     }
 
 }
